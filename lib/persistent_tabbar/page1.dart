7216f1503df46680582d969eee6949cdf7b0e586
samira bagheri <bagherisamira@ymail.com>
2021-09-12 15:34:43 +0430

    first

--- lib/persistent_tabbar/page1.dart
+++ lib/persistent_tabbar/page1.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_app1/persistent_tabbar/page2.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -8,84 +9,11 @@ class Page1 extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _Page1State extends State<Page1> {
-  var list = [];
-
-  _loadList() async {
-    final response = await http
-        .get(Uri.parse("https://jsonplaceholder.typicode.com/posts/"));
-    if (response.statusCode == 200) {
-      await Future.delayed(const Duration(seconds: 1));
-      if (mounted) {
-        setState(() {
-          list = json.decode(response.body) as List;
-        });
-      }
-    } else {
-      throw Exception('Failed to load posts');
-    }
-  }
-
-  @override
-  void initState() {
-    _loadList();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (BuildContext context, int index) {
-              final data = list[index];
-              return ListTile(
-                contentPadding: EdgeInsets.all(10.0),
-                title: Text(data['title']),
-                subtitle: Text(
-                  data['body'],
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-/* @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          child: ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (BuildContext context, int index) {
-              final data = list[index];
-              return ListTile(
-                contentPadding: EdgeInsets.all(10.0),
-                title: Text(data['title']),
-                subtitle: Text(
-                  data['body'],
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }*/
-}
-
 class _MyAppState extends State<Page1> {
   Color selection = new Color(0xF11162D);
-
+  onButtonTap(Widget page)  {
+    Navigator.push(this.context, MaterialPageRoute(builder: (BuildContext context) => page));
+  }
   @override
   Widget build(BuildContext context) {
     /*  Widget titleSection = Container(
@@ -127,39 +55,81 @@ class _MyAppState extends State<Page1> {
     );
     return titleSection;*/
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: new Scaffold(
+        backgroundColor: selection,
         body: new Container(
-
           child: new Column(
-
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-
-             Expanded(
-                  child: new Row(
+              new Row(
+                mainAxisSize: MainAxisSize.min,
                 children: [
-                  Container(
+                  new Container(
                     margin: EdgeInsets.all(10),
                     width: 50,
                     height: 50,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                          image: AssetImage('image/default_img.png'),
+                          image: AssetImage('assets/image/default_img.png'),
                           fit: BoxFit.fill),
                     ),
                   ),
-                  new Text("Samira Bagheri"),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: new Text("Samira Bagheri",
+                          style: TextStyle(color: Colors.white)))
                 ],
-              )),
-            //  new Text("Title List"),
-              new Text("Samira Bagheri" , style: TextStyle(
-                color: Colors.grey[500],)),
-              SizedBox(
-                height: 250, // constrain height
+              ),
+              new Container(
+                  margin: const EdgeInsets.all(20.0),
+                  child: Text("Recommendation",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold))),
+              new Container(
+                height: 300.0, // constrain height
                 child: ListViewHome(),
               ),
-           //   Expanded(child: ListViewHome()),
+              new Row(
+                children: [
+                  new Container(
+                      margin: EdgeInsets.all(20),
+                      padding: EdgeInsets.only(top: 30),
+                      child: Text("Specialty",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold))),
+                  Spacer(),
+                  new Row(
+                    children: [
+                      new Text("More",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold)),
+                      InkWell(onTap:() {
+                        onButtonTap(
+                          Page2(),
+                        );
+                      },      child: new Container(
+                        margin: EdgeInsets.all(20),
+                        child: Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
+                          size: 20.0,
+                        ),
+                      ) ),
 
+                    ],
+                  )
+                ],
+              ),
+              new Container(
+                height: 150.0, // constrain height
+                child: ListViewHomeSmall(),
+              ),
             ],
           ),
         ),
@@ -175,15 +145,89 @@ class ListViewHome extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       padding: const EdgeInsets.all(8),
       children: <Widget>[
-
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full)),
-        ListTile( title: Text("Battery Full"), leading: Icon(Icons.battery_full))
-
+        Container(
+          height: 300,
+          width: 300,
+          color: Colors.green[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 300,
+          width: 300,
+          color: Colors.purple[300],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          width: 100,
+          color: Colors.blue[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+        Container(
+          height: 300,
+          width: 300,
+          color: Colors.amber[200],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 300,
+          width: 300,
+          color: Colors.red[500],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 300,
+          width: 300,
+          color: Colors.red[200],
+          child: const Center(child: Text('Entry A')),
+        )
+      ],
+    );
+  }
+}
+class ListViewHomeSmall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 150,
+          width: 150,
+          color: Colors.green[600],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          color: Colors.purple[300],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 50,
+          width: 100,
+          color: Colors.blue[100],
+          child: const Center(child: Text('Entry C')),
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          color: Colors.amber[200],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          color: Colors.red[500],
+          child: const Center(child: Text('Entry A')),
+        ),
+        Container(
+          height: 150,
+          width: 150,
+          color: Colors.red[200],
+          child: const Center(child: Text('Entry A')),
+        )
       ],
     );
   }
