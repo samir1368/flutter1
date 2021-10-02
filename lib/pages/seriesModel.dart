36886b2737d5820e55a4b65688c16438d79b75e8
samira bagheri <bagherisamira@ymail.com>
2021-10-02 16:34:02 +0330

    RavitoonProj

---
+++ lib/pages/seriesModel.dart
@@ -0,0 +1,10 @@
+
+
+class seriesModel {
+  final String title;
+  final String summary;
+  final String imgUrl;
+  final int id;
+
+  const seriesModel(this.id,this.title, this.summary ,this.imgUrl);
+}
